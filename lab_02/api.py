3eefc7479f8701cfc705fe3222f96cebc6ff9de2
Byncute <kietho210698@gmail.com>
2025-05-23 18:20:47 +0700

    lab_02 transposition

--- lab_02/api.py
+++ lab_02/api.py
@@ -2,15 +2,19 @@ from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
-from cipher.playfair import PlayFairCipher  # ✅ Thêm dòng này
+from cipher.playfair import PlayFairCipher  
+from cipher.transposition import TranspositionCipher
+
 
 app = Flask(__name__)
 
-# Khởi tạo các đối tượng mã hóa
+
 caesar_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
 railfence_cipher = RailFenceCipher()
-playfair_cipher = PlayFairCipher()  # ✅ Khởi tạo đối tượng PlayFairCipher
+playfair_cipher = PlayFairCipher() 
+transposition_cipher = TranspositionCipher()
+
 
 # ========================== CAESAR CIPHER ==========================
 
@@ -96,6 +100,23 @@ def playfair_decrypt():
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, matrix)
     return jsonify({'decrypted_text': decrypted_text})
 
+# ===================== TRANSPOSITION CIPHER =====================
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 # ============================== RUN ===============================
 
