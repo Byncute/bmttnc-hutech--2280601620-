368e3458b25234faff1c41c3f62092a10f0fb881
Byncute <kietho210698@gmail.com>
2025-06-11 11:14:00 +0700

    [add]ungdungmahoa

---
+++ lab03/vigenere_cipher.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from ui.vigenere import Ui_MainWindow  # <-- Đảm bảo bạn có file .ui tương ứng và đã chuyển sang Python bằng pyuic5
+import requests
+
+class VigenereApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/vigenere/encrypt"
+        payload = {
+            "plain_text": self.ui.txt_plain_text.toPlainText(),
+            "key": self.ui.txt_key.text()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_cipher_text.setText(data["encrypted_text"])
+                self.show_message("Encrypted Successfully")
+            else:
+                self.show_message("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            self.show_message(f"Error: {str(e)}")
+
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/vigenere/decrypt"
+        payload = {
+            "cipher_text": self.ui.txt_cipher_text.toPlainText(),
+            "key": self.ui.txt_key.text()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_plain_text.setText(data["decrypted_text"])
+                self.show_message("Decrypted Successfully")
+            else:
+                self.show_message("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            self.show_message(f"Error: {str(e)}")
+
+    def show_message(self, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(message)
+        msg.exec_()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = VigenereApp()
+    window.show()
+    sys.exit(app.exec_())
